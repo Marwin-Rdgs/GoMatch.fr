f98b2b4bdcebe3b15d75865f097b602fb1d14db6
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2022-05-09 15:44:43 +0200

    Avancement Footer

--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,18 @@
       assumenda quisquam. Delectus voluptatem, quasi expedita incidunt aliquam
       tempora.
     </p>
+
+    <footer1 />
   </div>
 </template>
 
 
 <script >
 import header1 from "./components/header.vue";
+import footer1 from "./components/footer.vue";
 
 export default {
   name: "App",
-  components: { header1 },
+  components: { header1, footer1 },
 };
 </script>
---
+++ src/components/footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-12/12 bg-black text-white">
+    <div class="pt-7 footer_turquoise"></div>
+
+    <div class="flex ml-4">
+      <div class="flex-col w-1/2 pb-7">
+        <h3 class="mb-2">Meu :</h3>
+        <p>➤ À propos</p>
+        <p>➤ Contact</p>
+      </div>
+      <div class="flex-col w-1/2">
+        <h3 class="mb-2">Nous suivre :</h3>
+        <div class="flex">
+          <img src="./icones/footer_facebook.png" alt="" />
+          <img src="./icones/footer_instagram.png" alt="" />
+          <img src="./icones/footer_twitter.png" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
