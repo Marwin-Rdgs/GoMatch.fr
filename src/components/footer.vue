5c47e6f979c24eb666f27fda10a0bc8ea6b0f2f6
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2022-05-13 21:45:52 +0200

    Footer créé (amélioration en attente)

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="w-12/12 bg-black text-white">
+  <div class="w-full bg-black text-white">
     <div class="pt-7 footer_turquoise"></div>
 
-    <div class="flex ml-4">
+    <div class="flex ml-10 w-full">
       <div class="flex-col w-1/2 pb-7">
         <h3 class="mb-2">Meu :</h3>
         <p>➤ À propos</p>
         <p>➤ Contact</p>
       </div>
-      <div class="flex-col w-1/2">
-        <h3 class="mb-2">Nous suivre :</h3>
-        <div class="flex">
-          <img src="./icones/footer_facebook.png" alt="" />
-          <img src="./icones/footer_instagram.png" alt="" />
-          <img src="./icones/footer_twitter.png" alt="" />
+      <div class="flex-col place-items-end">
+        <h3 class="mb-2 contents">Nous suivre :</h3>
+        <div class="flex flex-auto">
+          <a href="#"><img src="./icones/footer_facebook.png" alt="" /></a>
+          <a href="#"
+            ><img src="./icones/footer_instagram.png" alt="" class="ml-2"
+          /></a>
+          <a href="#"
+            ><img src="./icones/footer_twitter.png" alt="" class="ml-2"
+          /></a>
         </div>
       </div>
     </div>
