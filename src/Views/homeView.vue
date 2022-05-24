74ebbdb97c5aafdd8c2a6cb894c1f22a71c7ae0c
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2022-05-24 16:13:19 +0200

    Index finis (sans anim, en commentaire)

--- src/Views/homeView.vue
+++ src/Views/homeView.vue
@@ -3,6 +3,14 @@ import sportindex from "../components/sportindex.vue"
 
 export default {
   name: "homeView",
+  // data: function {
+  //   return {
+  //       affiche:'',
+  //       indexMot:0,
+  //       posMot:0,
+  //       monteMot:true,
+  //   }
+  // },
   components: { sportindex }
 };
 
@@ -15,111 +23,112 @@ export default {
 // Scripts
 // 
 
-const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor");
+// const typedTextSpan = document.querySelector(".typed-text");
+// const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Football", "Basketball", "Pétanque", "Tennis"];
-const typingDelay = 100;
-const erasingDelay = 100;
-const newTextDelay = 50; // Delay between current and next text
-let textArrayIndex = 0;
-let charIndex = 0;
+// const textArray = ["Football", "Basketball", "Pétanque", "Tennis"];
+// const typingDelay = 100;
+// const erasingDelay = 100;
+// const newTextDelay = 50; // Delay between current and next text
+// let textArrayIndex = 0;
+// let charIndex = 0;
 
-function type() {
-  if (charIndex < textArray[textArrayIndex].length) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-  	setTimeout(erase, newTextDelay);
-  }
-}
+// function type() {
+//   if (charIndex < textArray[textArrayIndex].length) {
+//     if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+//     typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+//     charIndex++;
+//     setTimeout(type, typingDelay);
+//   } 
+//   else {
+//     cursorSpan.classList.remove("typing");
+//   	setTimeout(erase, newTextDelay);
+//   }
+// }
 
-function erase() {
-	if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if(textArrayIndex>=textArray.length) textArrayIndex=0;
-    setTimeout(type, typingDelay + 1100);
-  }
-}
+// function erase() {
+// 	if (charIndex > 0) {
+//     if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+//     typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+//     charIndex--;
+//     setTimeout(erase, erasingDelay);
+//   } 
+//   else {
+//     cursorSpan.classList.remove("typing");
+//     textArrayIndex++;
+//     if(textArrayIndex>=textArray.length) textArrayIndex=0;
+//     setTimeout(type, typingDelay + 1100);
+//   }
+// }
 
-document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-  if(textArray.length) setTimeout(type, newTextDelay + 100);
-});
+// document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+//   if(textArray.length) setTimeout(type, newTextDelay + 100);
+// });
 
-window.addEventListener('DOMContentLoaded', event => {
+// window.addEventListener('DOMContentLoaded', event => {
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+//     // Navbar shrink function
+//     var navbarShrink = function () {
+//         const navbarCollapsible = document.body.querySelector('#mainNav');
+//         if (!navbarCollapsible) {
+//             return;
+//         }
+//         if (window.scrollY === 0) {
+//             navbarCollapsible.classList.remove('navbar-shrink')
+//         } else {
+//             navbarCollapsible.classList.add('navbar-shrink')
+//         }
 
-    };
+//     };
 
-    // Shrink the navbar 
-    navbarShrink();
+//     // Shrink the navbar 
+//     navbarShrink();
 
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
+//     // Shrink the navbar when page is scrolled
+//     document.addEventListener('scroll', navbarShrink);
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 72,
-        });
-    };
+//     // Activate Bootstrap scrollspy on the main nav element
+//     const mainNav = document.body.querySelector('#mainNav');
+//     if (mainNav) {
+//         new bootstrap.ScrollSpy(document.body, {
+//             target: '#mainNav',
+//             offset: 72,
+//         });
+//     };
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
+//     // Collapse responsive navbar when toggler is visible
+//     const navbarToggler = document.body.querySelector('.navbar-toggler');
+//     const responsiveNavItems = [].slice.call(
+//         document.querySelectorAll('#navbarResponsive .nav-link')
+//     );
+//     responsiveNavItems.map(function (responsiveNavItem) {
+//         responsiveNavItem.addEventListener('click', () => {
+//             if (window.getComputedStyle(navbarToggler).display !== 'none') {
+//                 navbarToggler.click();
+//             }
+//         });
+//     });
 
-});
+// });
 
 </script>
 
 <template>
-<div class="screen">
-            <h1 class="title_minia"> <br> <br> GoMatch!</h1>
+<div class="screen w-full">
+            <h1 class="title_minia font-impact text-2xl text-center"> <br> <br> GoMatch!</h1>
             <div class="container fw-bolder">
-                <p class="title_anim"> Go <br>
-                <span class="typed-text"></span><span class="cursor">&nbsp;</span>
+                <p class="title_anim font-impact text-xl text-center"> Go <br>
+                <span class="typed-text"></span><span class="cursor">&nbsp;Football</span>
                 </p>
-                <a href=#down><img src="../components/icones/arrow_down.svg" alt="Icône descendre" class="arrow_down"></a>
+                <a href=#down><img src="../components/icones/arrow_down.svg" alt="Icône descendre" class="arrow_down py-9"></a>
             </div>
         </div>
 
             <sportindex 
     type="foot"
     txt="Football"
-    url="#" />
+    url="#"
+    id="down" />
 
     <hr class="w-1/6 m-auto"/>
 
@@ -182,13 +191,11 @@ hr { color:#6B7280; background-color:#6B7280; height:3px; border:0; }
 .title_minia
 {
     margin-top: 0px;
-    text-align: center;
     font-size: 100px;
     color: #FFFFFF;
 }
 
 .title_anim {
-    text-align: center;
     color: #FFFFFF;
     margin-bottom: 260px;
 }
