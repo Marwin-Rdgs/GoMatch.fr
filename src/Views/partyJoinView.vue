366b7046304d4e49acbc4d9208c6c03d47f6d70c
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2022-06-08 00:44:45 +0200

    Party join Modification Base

--- src/Views/partyJoinView.vue
+++ src/Views/partyJoinView.vue
@@ -4,7 +4,7 @@
     <h2 class="font-bold text-xl">Ville : Héricourt</h2>
 <div class="ml-5"><joueur/>
 <p class="text-3xs">Actuellement</p>
-<p class="bg-turquoise w-fit rounded-md text-white text-3xs">5 personne</p>
+<p class="bg-turquoise w-fit rounded-md text-white text-3xs p-1">5 personne</p>
 </div>
 </div>
     <hr class="m-4 w-40 bg-black h-0.5"/>
@@ -13,48 +13,44 @@
     <p class="text-3xs bg-white w-9/12 rounded-md">6 rue du Rohan, 70400</p>
     </div>
         <hr class="m-4 w-40 bg-black h-0.5"/>
-    <div class="m-4">
-        <p class="text-4xs">Sports praticables : </p>
-    <div class="bg-white rounded-lg w-9/12">
-                <div class="flex"><img
-        src="../components/icones/ballon_foot.png"
-        class="w-14 h-14 m-2"/>
-        <p class="mt-5">Football</p>
-                </div>
-
-                <div class="flex"><img
-        src="../components/icones/ballon_basket.png"
-        class="w-14 h-14 m-2"/>
-        <p class="mt-5">Basketball</p>
-                </div>
-            </div>
-    </div>
     <pub/>
         <hr class="m-4 w-40 bg-black h-0.5"/>
         <h3 class="text-lg font-bold ml-6">Rejoindre le terrain</h3>
         <p class="text-xs ml-14 mt-3">Arrivé dans :</p>
         <div class="grid grid-cols-3 mb-5 mt-3 ml-14 gap-5">
-        <button class="bg-white rounded-md w-20 text-center ">15 min</button>
-        <button class="bg-white rounded-md w-20 text-center ">30 min</button>
-        <button class="bg-white rounded-md w-20 text-center ">1h</button>
-        <button class="bg-white rounded-md w-20 text-center ">1h30</button>
-        <button class="bg-white rounded-md w-20 text-center ">2h</button>
+        <button class="bg-white rounded-md w-20 text-center focus:bg-green focus:text-white">15 min</button>
+        <button class="bg-white rounded-md w-20 text-center focus:bg-green focus:text-white">30 min</button>
+        <button class="bg-white rounded-md w-20 text-center focus:bg-green focus:text-white">1h</button>
+        <button class="bg-white rounded-md w-20 text-center focus:bg-green focus:text-white">1h30</button>
+        <button class="bg-white rounded-md w-20 text-center focus:bg-green focus:text-white">2h</button>
         </div>
 
 <div class="flex ml-14 mb-5 mt-3">
         <p>Avec</p>
-        <input type="text" placeholder="..." class=" mx-1 rounded-md bg-white text-black text-center w-8">
+        <input type="number" placeholder="..." class=" mx-1 rounded-md bg-white text-black text-center w-8 focus:bg-green focus:text-white">
 <p>personne(s)</p>
 </div>
 
-        <p class="text-xs ml-14 mt-3">Pour du :</p>
-        <div class="grid grid-cols-3 mt-3 mb-5 ml-14 gap-5">
-        <button class="bg-white rounded-md w-20 text-center ">Football</button>
-        <button class="bg-white rounded-md w-20 text-center ">Basketball</button>
+    <div class="m-4">
+        <p class="text-xs">Liste des joueurs : </p>
+        <!-- V-for => liste des joueurs -->
+        <div class="flex bg-white w-9/12 rounded-md gap-x-10">
+                <img src="../components/icones/pp-nothing.png" alt="">
+                <p class="text-3xs ">6 rue du Rohan, 70400</p>
         </div>
+    </div>
+        <hr class="m-4 w-40 bg-black h-0.5"/>
+
         
-<div class="place-items-center">
+<div class="flex justify-center pb-10">
         <button class="bg-green text-white rounded-md w-44 h-11">Rejoindre la partie</button>
+</div>
+
+<!-- Si joueur déjà dans la partie -->
+<div class="flex justify-center pb-10">
+        <div class="bg-red-600 text-white rounded-md w-44 h-11 text-4xs">
+             <p class="text-center mt-3"> Vous êtes déjà dans une partie </p>
+        </div>
 </div>
 </template>
 
