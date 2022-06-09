a030da73944b5ad059d38209925bbcb966cf0ad7
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2022-06-09 15:29:00 +0200

    Mis à jour pour mise en ligne

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import componentsView from '../Views/componentsView.vue'
 import homeView from '../Views/homeView.vue'
 import mentionslegalesView from '../Views/mentionslegalesView.vue'
 import aproposView from '../Views/aproposView.vue'
@@ -18,7 +17,6 @@ const router = createRouter ({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {path: '/', name: 'homeView', component: homeView},
-        {path: '/components', name: 'componentsView', component: componentsView},
         {path: '/mentionslegales', name: 'mentionslegalesView', component: mentionslegalesView},
         {path: '/apropos', name: 'aproposView', component: aproposView},
         {path: '/contact', name: 'contactView', component: contactView},
